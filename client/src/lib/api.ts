d677485ac604914beaf3167d6a5a88d713739288
malek chahbani <malekchahbanix@gmail.com>
2025-10-05 23:55:20 +0100

    new features ++

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,5 +1,6 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api';
+//const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://amigos-backend-ga2t.onrender.com';
 
 interface LoginResponse {
   _id: string;
