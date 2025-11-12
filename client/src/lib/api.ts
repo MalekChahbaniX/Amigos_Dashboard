a038f37c5cfc98c659dd4f5978865f20d501649d
malek chahbani <malekchahbanix@gmail.com>
2025-11-12 21:02:08 +0100

    fix url

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,6 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api';
-//const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://amigos-backend-ga2t.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
 interface LoginResponse {
   _id: string;
